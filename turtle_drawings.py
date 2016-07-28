5b61e0e6e3791dcfc8fd3568d8e51b10492071b4
Diba Mirza <dimirza@eng.ucsd.edu>
2016-07-27 19:35:23 -0700

    Starter code

---
+++ turtle_drawings.py
@@ -0,0 +1,30 @@
+import turtle
+
+def drawPicture():
+    ''' Draw a simple picture using turtle graphics '''
+
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)   
+
+
+def spiral(initialLength, angle, multiplier):
+    
+    return
+
+
+
+def tree(trunkLength, height):
+
+    return
+
+
+    
+def snowflake(sideLength, levels):
+    return
+
